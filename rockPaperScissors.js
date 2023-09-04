a5500c0a16b7dfaa7049b69600d62721a7928bd0
MichaelCavell <96754228+MichaelCavell@users.noreply.github.com>
2023-09-04 18:47:05 -0400

    Add win tally counter
    Next up--game winner announce

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -25,23 +25,23 @@ function playRound(playerSelection, computerSelection) {
     let playerChoice = playerLowerCase.charAt(0).toUpperCase() + playerLowerCase.slice(1);
 
     if (computerSelection === "Paper" && playerChoice === "Rock") {
-        return "You Lose! Paper beats rock";
+        return "You Lose! The Internet chose paper, which beats rock";
     } else if (computerSelection === "Scissors" && playerChoice === "Rock") {
-        return "You Win! Rock beats scissors";      
+        return "You Win! Rock beats the Internet's choice--scissors";      
     } else if (computerSelection === "Rock" && playerChoice === "Rock") {
-        return "Tie! You both selected rock";
+        return "Tie! You and the Internet both selected rock";
     } else if (computerSelection === "Paper" && playerChoice === "Paper") {
-        return "Tie! You both selected paper";
+        return "Tie! You and the Internet both selected paper";
     } else if (computerSelection === "Scissors" && playerChoice === "Paper") {
-        return "You Lose! Scissors beats paper";      
+        return "You Lose! The Internet chose scissors, which beats paper";      
     } else if (computerSelection === "Rock" && playerChoice === "Paper") {
-        return "You Win! Paper beats rock";
+        return "You Win! Paper beats the Internet's choice--rock";
     } else if (computerSelection === "Paper" && playerChoice === "Scissors") {
-        return "You Win! Scissors beats paper";
+        return "You Win! Scissors beats the Internet's choice--paper";
     } else if (computerSelection === "Scissors" && playerChoice === "Scissors") {
-        return "Tie! You both selected scissors";      
+        return "Tie! You and the Internet both selected scissors";      
     } else if (computerSelection === "Rock" && playerChoice === "Scissors") {
-        return "You Lose! Rock beats scissors";
+        return "You Lose! The Internet chose rock, which beats scissors";
     } else {
         return "hmmmmmmm...";
     }
@@ -120,10 +120,26 @@ function playRound(playerSelection, computerSelection) {
 } */
 
 document.addEventListener('DOMContentLoaded', function() {
-
     const rock = document.getElementById("rock");
     const paper = document.getElementById('paper');
     const scissors = document.getElementById('scissors');
+    const score = document.getElementById('score');
+    const roundResult = document.getElementById('result');
+
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function counter() {
+        let roundString = roundResult.textContent;
+        let firstFive = roundString.substring(0, 5);
+        if (firstFive === "You L") {
+            computerScore++;
+        } if (firstFive === "You W") {
+            humanScore++;
+        }
+        let currentScore = document.getElementById('score');
+        currentScore.textContent = `Human: ${humanScore} | Internet: ${computerScore}`;
+    }
 
     rock.addEventListener('click', function() {
         const userInput = "rock";
@@ -132,6 +148,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const resultDisplay = document.getElementById('result');
         resultDisplay.textContent = result;
     });
+    rock.addEventListener('click', counter);
 
     paper.addEventListener('click', function() {
         const userInput = "paper";
@@ -140,6 +157,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const resultDisplay = document.getElementById('result');
         resultDisplay.textContent = result;
     });
+    paper.addEventListener('click', counter);
 
     scissors.addEventListener('click', function() {
         const userInput = "scissors";
@@ -148,6 +166,6 @@ document.addEventListener('DOMContentLoaded', function() {
         const resultDisplay = document.getElementById('result');
         resultDisplay.textContent = result;
     });
+    scissors.addEventListener('click', counter);
 });
-
 
