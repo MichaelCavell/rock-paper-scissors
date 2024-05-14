3d92b6037cff0403c0825748a80d1a012244ba41
MichaelCavell <96754228+MichaelCavell@users.noreply.github.com>
2024-05-14 16:26:38 -0400

    Add GUI, CSS, and update gameplay
    Added CSS styling and updated JS and HTML to
    enhance the gameplay experience.
    
    Might want to circle back eventually clean up and
    refactor JS and update styling but for the purposes
    of this project and TOP curriculum this feels like
    a good stopping point.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let computerScore = 0;
+let userScore = 0;
+let rounds = 0;
 
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
@@ -11,110 +14,192 @@ function getComputerChoice() {
     }
 }
 
-function singleRound(playerSelection, computerSelection) {
+function singleRound(playerSelection, computerChoice) {
     let playerChoice = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
-    let computerChoice = computerSelection();
-    console.log(playerChoice);
-    console.log(computerChoice);
 
     if (playerChoice === 'Rock') {
         switch (computerChoice) {
             case 'Rock':
-                return 'Tie! Both players chose Rock';
+                return `Tie! Both players chose Rock`;
             case 'Paper':
-                return 'You Lose! Paper beats Rock';
+                return `You lose round ${rounds}! Paper beats Rock`;
             case 'Scissors':
-                return 'You Win! Rock beats Scissors';
+                return `You win round ${rounds}! Rock beats Scissors`;
         }
     } else if (playerChoice === 'Paper') {
         switch (computerChoice) {
             case 'Rock':
-                return 'You Win! Paper beats Rock';
+                return `You win round ${rounds}! Paper beats Rock`;
             case 'Paper':
-                return 'Tie! Both players chose Paper';
+                return `Tie! Both players chose Paper`;
             case 'Scissors':
-                return 'You Lose! Scissors beats Paper';
+                return `You lose round ${rounds}! Scissors beats Paper`;
         }
     } else {
         switch (computerChoice) {
             case 'Rock':
-                return 'You Lose! Rock beats Scissors';
+                return `You lose round ${rounds}! Rock beats Scissors`;
             case 'Paper':
-                return 'You Win! Scissors beats Paper';
+                return `You win round ${rounds}! Scissors beats Paper`;
             case 'Scissors':
-                return 'Tie! Both players chose Scissors'; 
+                return `Tie! Both players chose Scissors`; 
         }
     }
 }
 
 function winnerCheck(string) {
-    if (string.includes('You Lose!')) {
+    if (string.includes('You lose')) {
+        computerScore++;
         return 1;
-    } else if (string.includes('You Win!')) {
+    } else if (string.includes('You win')) {
+        userScore++;
         return 2;
     } else { 
         return 3;
     }
 }
 
-function gameWinnerCheck(computerScore, userScore) {
-    if (computerScore > userScore) {
-        return 1;
-    } else if (computerScore < userScore) {
-        return 2;
-    } else {
-        return 3;
+////////////////////////
+
+const matchButton = document.querySelector('.match-button');
+const description = document.querySelector('.description');
+const playerButtons = document.querySelector('.player').children;
+const computerDivs = document.querySelector('.computer').children;
+const playerScoreDisplay = document.querySelector('#player-score');
+const computerScoreDisplay = document.querySelector('#computer-score');
+const playerRecap = document.querySelector('.player-scores');
+const computerRecap = document.querySelector('.computer-scores');
+const selectionArea = document.querySelector('#selection-area');
+const scoreboard = document.querySelector('#scoreboard');
+
+matchButton.addEventListener('click', startGame);
+matchButton.addEventListener('click', clearChoices);
+
+function startGame() {
+    for (let button of playerButtons) {
+        button.addEventListener('click', playRound)
+        button.classList.remove('selected', 'winner', 'loser');
+    }
+
+    for (let div of computerDivs) {
+        div.classList.remove('selected', 'winner', 'loser');
+    }
+
+    computerScore = 0;
+    userScore = 0;
+    rounds = 0;
+
+    playerScoreDisplay.textContent = `Player: ${userScore}`;
+    computerScoreDisplay.textContent = `Computer: ${computerScore}`;
+    selectionArea.style.visibility = 'visible';
+    scoreboard.style.visibility = 'visible';
+    matchButton.style.display = 'none';
+    description.textContent = `Click below to choose 🪨 📄 or ✂️ !`
+    playerScoreDisplay.style.backgroundColor = 'transparent';
+    computerScoreDisplay.style.backgroundColor = 'transparent';
+}
+
+function endGame() {
+    for (let button of playerButtons) {
+        matchButton.style.display = 'inline';
+        matchButton.textContent = 'Play Again!';
+        button.removeEventListener('click', playRound)
     }
 }
 
-function playGame() {
-    let computerScore = 0;
-    let userScore = 0;
-
-    // for (let i = 0; i < 5; i++) {
-    //     let userMove = prompt('Rock, Paper, or Scissors?')
-    //     let currentRound = singleRound(userMove, getComputerChoice);
-    //     console.log(currentRound);
-    //     let roundWinner = winnerCheck(currentRound);
-    //     switch (roundWinner) {
-    //         case 1:
-    //             computerScore++
-    //             break;
-    //         case 2:
-    //             userScore++
-    //             break;      
-    //     } 
-    //     console.log(`The current score is Computer: ${computerScore} | User(you!): ${userScore}`);
-    // }
-
-    let gameWinner = gameWinnerCheck(computerScore, userScore);
-    switch (gameWinner) {
-        case 1:
-            console.log(`You lose! The final score is Computer: ${computerScore} | User(you!): ${userScore}`)
-            console.log(`Refresh to play again!`);
-            break;
-        case 2:
-            console.log(`You win! The final score is Computer: ${computerScore} | User(you!): ${userScore}`);
-            console.log(`Refresh to play again!`);
-            break;  
-        case 3:  
-            console.log(`You tie! The final score is Computer: ${computerScore} | User(you!): ${userScore}`);
-            console.log(`Refresh to play again!`);
-            break;  
-    } 
+function playRound(e) {
+    rounds++;
+    const playerChoice = e.target.dataset.selection;
+    const computerChoice = getComputerChoice()
+    const roundResult = singleRound(playerChoice, computerChoice);
+    const winner = winnerCheck(roundResult);
+    if (winnerCheck == 1) computerScore++;
+    if (winnerCheck == 2) userScore++;
+    updateSelectionStyling(playerChoice, computerChoice, winner);
+    description.textContent = `${roundResult}--Click 🪨 📄 or ✂️ below to make your choice
+    for the next round!`
+    playerScoreDisplay.textContent = `Player: ${userScore}`;
+    computerScoreDisplay.textContent = `Computer: ${computerScore}`;
+    updateRoundRecap(playerChoice, computerChoice, winner);
+    if (computerScore == 5) {
+        endGame()
+        playerScoreDisplay.style.backgroundColor = 'red';
+        computerScoreDisplay.style.backgroundColor = 'green';
+        description.textContent = `After ${rounds} rounds, the COMPUTER is the champion!`
+    }
+    if (userScore == 5) {
+        endGame()
+        playerScoreDisplay.style.backgroundColor = 'green';
+        computerScoreDisplay.style.backgroundColor = 'red';
+        description.textContent = `After ${rounds} rounds, YOU are the champion!`  
+    }
 }
 
-playGame();
+function updateSelectionStyling(playerChoice, computerChoice, winnerCheck) {
+    for (let button of playerButtons) {
+        button.classList.remove('selected', 'winner', 'loser');
+        if (button.dataset.selection.toLowerCase() == playerChoice.toLowerCase()) {
+            if (winnerCheck == 1) {
+                button.classList.add('loser');
+            } else if (winnerCheck == 2) {
+                button.classList.add('winner');
+            } else {
+                button.classList.add('selected');
+            }
+        }
+    }
 
-////////////////////////
-const matchButton = document.querySelector('.match-button');
-matchButton.addEventListener('click', playGame);
-
-const playerButtons = document.querySelector('.player').children;
-for (let button of playerButtons) {
-    button.addEventListener('click', (e) => {
-        const playerChoice = e.target.dataset.selection;
-        singleRound(playerChoice, getComputerChoice);
-    })
+    for (let div of computerDivs) {
+        div.classList.remove('selected', 'winner', 'loser');
+        if (div.dataset.selection.toLowerCase() == computerChoice.toLowerCase()) {
+            if (winnerCheck == 1) {
+                div.classList.add('winner');
+            } else if (winnerCheck == 2) {
+                div.classList.add('loser');
+            } else {
+                div.classList.add('selected');
+            }
+        }
+    }
 }
 
+function updateRoundRecap(playerChoice, computerChoice, winner) {
+    function emojiConversion(string) {
+        if (string.toLowerCase() === 'rock') {
+            return '🪨'
+        } else if (string.toLowerCase() === 'paper') {
+            return '📄'
+        } else {
+            return '✂️'
+        }
+    }
+
+    const playerP = document.createElement('p');
+    const computerP = document.createElement('p');
+    playerP.classList.add('round-choice');
+    computerP.classList.add('round-choice');
+    
+    if (winner == 1) {
+        playerP.style.backgroundColor = 'red';
+        computerP.style.backgroundColor = 'green';
+    }
+
+    if (winner == 2) {
+        playerP.style.backgroundColor = 'green';
+        computerP.style.backgroundColor = 'red';
+    }
+
+    playerP.textContent = `Round ${rounds}: ${emojiConversion(playerChoice)}`
+    computerP.textContent = `Round ${rounds}: ${emojiConversion(computerChoice)}`
+
+    playerRecap.appendChild(playerP);
+    computerRecap.appendChild(computerP);
+}
+
+function clearChoices() {
+    const roundChoices = document.querySelectorAll('.round-choice');
+
+    for (let i = 0; i < roundChoices.length; i++) {
+        roundChoices[i].parentNode.removeChild(roundChoices[i])
+    }
+}
